f9192a40d579c8047a812604d287f0739bd255dc
Anny He <anny.he@salesforce.com>
2019-06-21 15:12:34 -0700

    on related pdf, if no related PDF files, show conditional markup

--- force-app/main/default/lwc/showPdfRelatedToRecordId/showPdfRelatedToRecordId.js
+++ force-app/main/default/lwc/showPdfRelatedToRecordId/showPdfRelatedToRecordId.js
@@ -8,7 +8,7 @@ export default class ShowPdfRelatedToRecordId extends LightningElement {
 
     @wire(getRelatedFilesByRecordId, { recordId: '$recordId' })
     wiredFieldValue({ error, data }) {
-        if (data) {
+        if (data && JSON.stringify(data) !== '[]') {
             this.pdfFileIds = data;
             this.error = '';
         } else if (error) {
