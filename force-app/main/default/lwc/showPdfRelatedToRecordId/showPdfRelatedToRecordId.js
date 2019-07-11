cde55b9fa1e98eec72c6095ddfa893249eb8e148
Anny He <anny.he@salesforce.com>
2019-07-11 08:24:55 -0700

    add error component and comments

--- force-app/main/default/lwc/showPdfRelatedToRecordId/showPdfRelatedToRecordId.js
+++ force-app/main/default/lwc/showPdfRelatedToRecordId/showPdfRelatedToRecordId.js
@@ -2,26 +2,30 @@ import { LightningElement, api, track, wire } from 'lwc';
 import getRelatedFilesByRecordId from '@salesforce/apex/PDFViewerController.getRelatedFilesByRecordId';
 
 export default class ShowPdfRelatedToRecordId extends LightningElement {
+    // Current record ID. *recordId* is a reserved identifier
     @api recordId;
+    // Declare to pass heightInRem to the child component in markup
     @api heightInRem;
-    @track pdfFiles = [];
-    @track error;
+    // Specify which file for child component to render
     @track fileID;
+    pdfFiles = [];
 
     @wire(getRelatedFilesByRecordId, { recordId: '$recordId' })
     wiredFieldValue({ error, data }) {
         if (data) {
             this.pdfFiles = data;
-            this.error = '';
+            this.error = undefined;
+            // Save the first related PDF's file ID to fileID            
             const fileIDs = Object.keys(data);
-            this.fileID =  fileIDs.length ? fileIDs[0] : ''; 
+            this.fileID =  fileIDs.length ? fileIDs[0] : undefined; 
         } else if (error) {
             this.error = error;
-            this.pdfFiles = ''; 
-            this.fileID = ''; 
+            this.pdfFiles = undefined; 
+            this.fileID = undefined; 
         }
     }
 
+    // Maps file ID and title to tab value and label
     get tabs() {
         if (!this.fileID) return [];
 
@@ -36,6 +40,7 @@ export default class ShowPdfRelatedToRecordId extends LightningElement {
         return tabs;
     }
 
+    // event handler for each tab: onclick tab, change fileID
     setFileID(e) {
         this.fileID = e.target.value;
     }
